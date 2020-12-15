a4c371f8c30979a1d32a5024d5b464313cd9caac
Sheyin <Sheyin@users.noreply.github.com>
2020-12-15 14:51:15 -0600

    Day2 still, troubleshooting regex

--- day2.py
+++ day2.py
@@ -1,3 +1,5 @@
+import re
+
 filename = "day2input.txt"
 data = []
 validPasswordCount = 0
@@ -6,20 +8,28 @@ input = open(filename, "r")
 
 for _ in input:
     line = _.rstrip()
-    splitLine = line.split(": ")
+    splitLine = re.split(r'[: -]', line)
+    splitLine.remove("")
 
     # Parsing number range
-    rangeSplit = splitLine[0].split("-")
-    minQuantity = int(rangeSplit(0))
-    maxQuantity = int(rangeSplit(1))
-
-    letterRequired = splitLine[1]
-
-    password = splitLine[2]
+    minQuantity = splitLine[0]
+    maxQuantity = splitLine[1]
+    letterRequired = splitLine[2]
+    password = splitLine[3]
 
     # At this point the data should be neatly organized
     # minQuantity, maxQuantity - int values for number of letterRequired
     # letterRequired - the letter in question required in password
     # password - the string to check for the specified letter range
 
+    # Assembling regex pattern - I'm not sure if I can do this in one line
+    pattern = letterRequired + "{" + minQuantity + "," + maxQuantity + "}"
+
     # Now to do the checking....
+    if re.search(pattern, password):
+        validPasswordCount += 1
+        print("A valid password: " + str(re.search(pattern, password)) +
+              " pattern: " + pattern + " password: " + password)
+    # Should return None if not found, so it shouldn't trigger statement.
+
+print("Final results: " + str(validPasswordCount) + " valid passwords.")
