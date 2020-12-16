6455714756442cf1d8df1977125fe17a4d9080d7
Sheyin <Sheyin@users.noreply.github.com>
2020-12-16 13:56:26 -0600

    Completed Part one!

--- day5.py
+++ day5.py
@@ -13,29 +13,33 @@ def main():
     for _ in input:
         line = _.rstrip()
         boarding_passes.append(line)
-        # There are 128 rows, ranging from 0 to 127
-
     # boarding_passes should now be a tidy array of data
-    # Initial range
-    range = (1, 128)
-    # This is the example given on the website.  Using it to debuf splitSeat()
-    boarding_pass = "FBFBBFFRLR"
-    for char in boarding_pass:
-        range = splitSeat(char, range)
-        print(f'{char}: {range}')
+
+    highest_seat_pid = 0
+    for boarding_pass in boarding_passes:
+        seat_info = {}
+        seat = locateSeat(boarding_pass)
+        seat_info["row"] = seat[0]
+        seat_info["col"] = seat[1]
+        seat_info["pid"] = (seat[0] * 8) + seat[1]
+        seat_info["pattern"] = boarding_pass
+        if seat_info["pid"] > highest_seat_pid:
+            highest_seat_pid = seat_info["pid"]
+    print(f"Part One: Highest Seat PID is: {highest_seat_pid}")
 
 
-# For part 1 - calculates the seat's row/col position based on rules.
-# Returns an integer (hopefully, the seat location)
-# There are 128 rows, ranging from 0 to 127 - F/B used to calculate
-# F: lower half
-# B: upper half
-# Last 3 chars will be L/R to calculate column #, but ranging from 0-7 (8 total)
-# L: lower half
-# R: upper half
+# Calculates the seat's row/col position based on rules.
+# Return a tuple (row, column)
 def locateSeat(boarding_pass):
-    for _ in boarding_pass:
-        pass
+    row_pattern = boarding_pass[:-3]
+    col_pattern = boarding_pass[-3:]
+    row_range = (0, 127)
+    for char in row_pattern:
+        row_range = splitSeat(char, row_range)
+    col_range = (0, 7)
+    for char in col_pattern:
+        col_range = splitSeat(char, col_range)
+    return (row_range[0], col_range[0])
 
 
 # Helper function for locateSeat.
@@ -45,21 +49,18 @@ def locateSeat(boarding_pass):
 def splitSeat(letter, range):
     minSeat = range[0]
     maxSeat = range[1]
+    difference = int((maxSeat - minSeat) / 2)
+    lowerHalf = (minSeat, minSeat + difference)
+    upperHalf = (maxSeat - difference, maxSeat)
+
     # Keep upper half
     if letter == "B" or letter == "R":
-        newMinSeat = int(maxSeat / 2)
-        #print("newMinseat: " + str(newMinSeat))
-        return (newMinSeat, maxSeat)
+        return upperHalf
     # Keep lower half
-    # elif letter == "F" or letter == "L":
-    else:
-        newMaxSeat = int(minSeat + maxSeat / 2)
-        #print("newMaxseat: " + str(newMaxSeat))
-        return (minSeat, newMaxSeat)
-    '''
+    elif letter == "F" or letter == "L":
+        return lowerHalf
     else:
         print("Some error occurred - neither F/B/L/R")
-    '''
 
 
 if __name__ == "__main__":
