85e691cc5a140ed831f63be03425790d6141dc4d
Sheyin <Sheyin@users.noreply.github.com>
2020-12-17 17:34:07 -0600

    Finally figured out part 1

--- day7.py
+++ day7.py
@@ -1,58 +1,50 @@
 import json
 
-
-def main():
-    rules = getData()
-    can_contain_gold_count = count_which_can_contain("shiny gold", rules)
-    print(
-        f"Part one: {can_contain_gold_count} bag colors can hold shiny gold bags.")
-
-
-# Part one- counts how many total rules(colors) allow for a specified
-# bag color can be placed inside of another.
-# Requires the rules dict and returns an integer (count).
-def count_which_can_contain(color, rules):
-    count_bags = 0
-    for bag_type in rules.keys():
-        # This bag can hold the color directly
-        if bag_can_contain(color, rules[bag_type]):
-            count_bags += 1
-        # Otherwise, search to see if they can be held indirectly (bag-in-bag-in-bag)
-        else:
-            for bag in rules[bag_type].keys():
-                if bag_can_contain(color, rules[bag]):
-                    count_bags += 1
-                    # This is required to stop checking for multiple instances
-                    break
-    return count_bags
-
-
-# This belongs in another function, but for brainstorming
+gold = "shiny gold"
 searched_bags = {
     "has_gold": [],  # List all the bags that can in/directly hold gold
     "no_gold": []   # list all the colors that cannot hold gold
 }
-# So, searched_bags["has_gold"] will eventually contain the final result (colors, len=count)
+rules = {}
 
 
-# Recursion time - need to search sub-bags and track which have been searched
-def recursive_bag_search(color, searched_bags, rules, looking_for="shiny gold"):
-    # End conditions
-    # This might not be needed, actually. (caught by normal process)
-    # if rules[color] == {}:
-    #   return
-    if color in searched_bags["no_gold"] or color in searched_bags["has_gold"]:
-        return
-    # General case -not yet searchec
-    else:
-        # This color was not yet searched - check if gold is in here
-        if bag_can_contain(looking_for, rules[color]):
+def main():
+    global rules
+    rules = getData()
+    search_bags_for_gold()
+    print(
+        f'Part one: {len(searched_bags["has_gold"])} bags can eventually hold {gold} bags.')
+
+
+# This just populates the searched_bags dict with the
+# search results whether it can hold gold bags (sorting function)
+def search_bags_for_gold():
+    # Populate searched bags with initial top level results
+    # Before calling recursive function
+    for color in rules.keys():
+        if bag_can_contain(gold, rules[color]):
             searched_bags["has_gold"].append(color)
-            return
-        # This color doesn't have gold listed - check deeper
-        else:
-            for _ in rules[color].keys():
-                recursive_bag_search(_, searched_bags, rules)
+    # Run loop again but lets just search for everything
+    # I'm sure this is probably not quite correct.
+    for color in rules.keys():
+        search_bags_for_color(color)
+
+
+# Recursive function - searches if a bag can eventually hold gold
+# Mostly just to populate searched_bags and sort the results.
+def search_bags_for_color(color):
+    if color in searched_bags["has_gold"]:
+        return True
+    elif color in searched_bags["no_gold"]:
+        return False
+    else:
+        for sub_color in rules[color].keys():
+            if search_bags_for_color(sub_color):
+                searched_bags["has_gold"].append(color)
+                return True
+        # Searched all bags for colors - not found among sub colors
+        searched_bags["no_gold"].append(color)
+        return False
 
 
 # Helper function that looks in a rule for a specific color (dict) and looks
